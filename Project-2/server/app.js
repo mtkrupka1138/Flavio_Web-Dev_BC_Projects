507f1732f4abfcdb5b55ecf697f297466088e5a9
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-05-25 14:20:28 -0400

    signup form data to server set up

--- Project-2/server/app.js
+++ Project-2/server/app.js
@@ -6,7 +6,40 @@ app.get('/', (req, res) => res.send('Hello from the server!'))
 
 app.post('/signup', (req, res) => {
     const email = req.body.email
+    const password = req.body.password
+    const password_confirmation = req.body.password_confirmation
+
+    if (!email || !password || !password_confirmation) {
+        res.status(400).send('Missing data')
+        return
+    }
+
+    if (accountExists(email)) {
+        res.status(403).send('Account already exists')
+        return
+    }
+
+    if (password !== password_confirmation) {
+        res.status(400).send('Passwords do not match')
+        return
+    }
+
+    if (password.length < 8) {
+        res.status(400).send('Password is invalid')
+        return
+    }
+
+    createAccount(email, password)
+    res.status(200).send('Account created')
 })
+
+const accountExists = (email) => {
+    return false
+}
+
+const createAccount = (email, password) => {
+    
+}
 
 
 app.post('/login', (req, res) => {
