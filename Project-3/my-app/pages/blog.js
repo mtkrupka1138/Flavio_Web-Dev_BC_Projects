a3edc20130869b5ab9211d1c2ac89a0e44dc55be
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-05-25 17:17:06 -0400

    router for blog posts

--- Project-3/my-app/pages/blog.js
+++ Project-3/my-app/pages/blog.js
@@ -1,7 +1,22 @@
+import Link from 'next/Link'
+import posts from '../../posts.json'
+
 export default function Blog() {
     return (
         <div>
             <h1>Blog</h1>
+
+            <ul>
+                {Object.keys(posts).map((id, index) => {
+                    return (
+                        <li key={index}>
+                            <Link href='/blog/[id]' as={'/blog/' + id}>
+                                <a>{posts[id].title}</a>
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
