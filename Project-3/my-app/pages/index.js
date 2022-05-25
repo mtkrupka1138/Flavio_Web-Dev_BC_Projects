fda144a35e1dae23eaa34ee7fce9b60584a84686
Meghan Krupka <mkrupka.hdm@gmail.com>
2022-05-25 16:39:15 -0400

    link blog

--- Project-3/my-app/pages/index.js
+++ Project-3/my-app/pages/index.js
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
+      <Link href='/blog'>
+        <a>Blog</a>
+      </Link>
     </div>
   )
 }
